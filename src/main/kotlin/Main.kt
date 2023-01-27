de6e28e1c4a8eeeb03b3192d2061db9143709061
eferreyr <emilia@ferreyra.org>
2023-01-27 08:56:13 -0600

    pretty much finished parser

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
 import it.skrape.core.htmlDocument
 import it.skrape.fetcher.*
+import it.skrape.selects.ElementNotFoundException
+import it.skrape.selects.eachText
 import it.skrape.selects.html5.*
 import java.io.File
+import java.text.DecimalFormat
+import java.text.NumberFormat
+import java.util.*
 
 /* CSS Selectors */
 const val BODY = "body.logged-in.javascript"
@@ -13,15 +18,66 @@ const val FIC_ELEMENT = "li.reading.work.blurb.group"
 const val FIC_HEADER = "div.header.module"
 const val TITLE_AND_AUTHOR_HEADER = "h4.heading"
 const val FIC_AUTHOR = "a[rel='author']"
+const val FANDOM_HEADER = "h5.fandoms.heading"
+const val TAG = "a.tag"
+const val DATE_LAST_UPDATED = "p.datetime"
+const val TAG_SECTION = "ul.tags.commas"
+const val WARNING_TAG = "li.warnings"
+const val RELATIONSHIP_TAG = "li.relationships"
+const val CHARACTER_TAG = "li.characters"
+const val FREEFORM_TAG = "li.freeforms"
+const val SUMMARY_BLOCK = "blockquote.userstuff.summary"
+const val SERIES_FOOTER = "ul.series"
+const val STATS_FOOTER = "dl.stats"
+const val LANGUAGE = "dd.language"
+const val WORD_COUNT = "dd.words"
+const val CHAPTER_COUNT = "dd.chapters"
+const val COMMENTS_COUNT = "dd.comments"
+const val KUDOS_COUNT = "dd.kudos"
+const val BOOKMARKS_COUNT = "dd.bookmarks"
+const val HITS_COUNT = "dd.hits"
 
 data class Fic(
     var id: String = "",
+    var header: Header = Header(),
+    var tags: Tags = Tags(),
+    var summary: String = "",
+    var isSeries: Boolean = false,
+    var series: Series? = null,
+    var stats: Stats = Stats()
+)
+
+data class Header(
     var title: String = "",
-    var author: String = ""
+    var author: List<String> = listOf(),
+    var fandom: List<String> = listOf(),
+    var dateLastUpdated: String = ""
+)
+
+data class Tags(
+    var warningTag: String = "",
+    var relationshipTags: List<String> = listOf(),
+    var characterTags: List<String> = listOf(),
+    var freeformTags: List<String> = listOf()
+)
+
+data class Series(
+    var name: String = "",
+    var part: Int = 0
+)
+
+data class Stats(
+    var language: String = "",
+    var wordCount: Int = 0,
+    var chapterCount: Int = 0,
+    var commentCount: Int = 0,
+    var kudosCount: Int = 0,
+    var bookmarkCount: Int = 0,
+    var hitCount: Int = 0
 )
 
 fun main(args: Array<String>) {
-    val ao3Client = skrape(BrowserFetcher) {
+    val ficList = skrape(BrowserFetcher) {
         request {
             url = "https://archiveofourown.org/users/itsOnNetflix/readings"
         }
@@ -34,7 +90,8 @@ fun main(args: Array<String>) {
                              MAIN_DASHBOARD {
                                  LIST_OF_FICS {
                                      FIC_ELEMENT {
-                                        findAll{ this } }.map{ processFic(it.toString()) }
+                                        findAll{ this }
+                                     }.map{ processFic(it.toString()) }
                                 }
                             }
                         }
@@ -44,7 +101,7 @@ fun main(args: Array<String>) {
         }
     }
 
-    print(ao3Client[0])
+    print(ficList[0])
 }
 
 fun processFic(rawFic: String): Fic {
@@ -53,22 +110,169 @@ fun processFic(rawFic: String): Fic {
 
     htmlDocument(rawFic) {
         li {
-            fic.id = findFirst { id }?.split("_")?.last() ?: "*"
-             FIC_HEADER {
-                 TITLE_AND_AUTHOR_HEADER {
-                     val ficTitle = "a[href='https://archiveofourown.org/works/${fic.id}']"
-                     
-                     ficTitle {
-                        fic.title = findFirst { text }
-                    }
+            fic.id = findFirst { id }.split("_").last()
 
-                     FIC_AUTHOR {
-                        fic.author = findFirst { text }
-                    }
+            FIC_HEADER {
+                fic.header = processHeader(findFirst { this }.toString(), fic.id)
+            }
+
+            TAG_SECTION {
+                fic.tags = processTags(findFirst { this }.toString())
+            }
+
+            SUMMARY_BLOCK {
+                p {
+                    fic.summary = findFirst { text }
                 }
+            }
+
+            try {
+                SERIES_FOOTER {
+                    fic.series = processSeries(findFirst { this }.toString())
+                    fic.isSeries = true
+                }
+            } catch (e: ElementNotFoundException) {}
+
+            STATS_FOOTER {
+                fic.stats = processStats(findFirst { this }.toString())
             }
         }
     }
 
     return fic
+}
+
+fun processHeader(rawHeader: String, id: String): Header {
+    val header = Header()
+
+    htmlDocument(rawHeader) {
+        TITLE_AND_AUTHOR_HEADER {
+            val ficTitle = "a[href='https://archiveofourown.org/works/${id}']"
+
+            ficTitle {
+                header.title = findFirst { text }
+            }
+
+            FIC_AUTHOR {
+                header.author = findAll { eachText }
+            }
+        }
+
+        FANDOM_HEADER {
+            TAG {
+                header.fandom = findAll { eachText }
+            }
+        }
+
+        DATE_LAST_UPDATED {
+            header.dateLastUpdated = findFirst { text }
+        }
+    }
+
+    return header
+}
+
+fun processTags(rawTagSection: String): Tags {
+//    println(rawTagSection)
+    val tags = Tags()
+
+    htmlDocument(rawTagSection) {
+        relaxed = true
+
+        WARNING_TAG {
+            strong {
+                TAG {
+                    tags.warningTag = findFirst { text }
+                }
+            }
+        }
+
+        RELATIONSHIP_TAG {
+            TAG {
+                tags.relationshipTags = findAll { eachText }
+            }
+        }
+
+        CHARACTER_TAG {
+            TAG {
+                tags.characterTags = findAll { eachText }
+            }
+        }
+
+        FREEFORM_TAG {
+            TAG {
+                tags.freeformTags = findAll { eachText }
+            }
+        }
+    }
+
+    return tags
+}
+
+fun processSeries(rawSeries: String): Series {
+    val series = Series()
+
+    htmlDocument(rawSeries) {
+        strong {
+            val numberFormat = NumberFormat.getNumberInstance(Locale.ENGLISH) as DecimalFormat
+            series.part = numberFormat.parse(findFirst { text }).toInt()
+        }
+
+        a {
+            series.name = findFirst { text }
+        }
+    }
+
+    return series
+}
+
+fun processStats(rawStats: String): Stats {
+    val stats = Stats()
+
+    htmlDocument(rawStats) {
+        val numberFormat = NumberFormat.getNumberInstance(Locale.ENGLISH) as DecimalFormat
+
+        LANGUAGE {
+            stats.language = findFirst { text }
+        }
+
+        WORD_COUNT {
+            stats.wordCount = numberFormat.parse(findFirst { text }).toInt()
+        }
+
+        CHAPTER_COUNT {
+            try {
+                a {
+                    stats.chapterCount = numberFormat.parse(findFirst { text }).toInt()
+                }
+            } catch (e: ElementNotFoundException) {
+                stats.chapterCount = 1
+            }
+
+        }
+
+        COMMENTS_COUNT {
+            a {
+                stats.commentCount = numberFormat.parse(findFirst { text }).toInt()
+            }
+        }
+
+        KUDOS_COUNT {
+            a {
+                stats.kudosCount = numberFormat.parse(findFirst { text }).toInt()
+            }
+        }
+
+        BOOKMARKS_COUNT {
+            a {
+                stats.bookmarkCount = numberFormat.parse(findFirst { text }).toInt()
+            }
+        }
+
+        HITS_COUNT {
+            stats.hitCount = numberFormat.parse(findFirst { text }).toInt()
+        }
+    }
+
+    return stats
 }
